549d5d6940809edc9539d8ad8de327fda18ea41c
Tun <tunkedsaro@gmail.com>
2024-08-30 17:05:30 +0700

    add about,real time,upload_image,video_extract

--- app_general/urls.py
+++ app_general/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from app_general import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('realtime',views.realtime,name='realtime'),
+    path('uploadimage',views.upload_image,name='upload_image'),
+    path('videoextract',views.video_extract,name='video_extract'),
 ]
--- app_general/views.py
+++ app_general/views.py
@@ -5,3 +5,15 @@ from django.http.response import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'app_general/home.html')
+
+def about(request):
+    return render(request,'app_general/about.html')
+
+def realtime(request):
+    return render(request,'app_general/realtime.html')
+
+def upload_image(request):
+    return render(request,'app_general/upload_image.html')
+
+def video_extract(request):
+    return render(request,'app_general/video_extract.html')
