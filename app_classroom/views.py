93931478f37c98769ec41fc10294d81aab01492b
Tun <tunkedsaro@gmail.com>
2024-08-31 11:22:14 +0700

    Insert 6 dummy data,Classrooms/,classroom/

--- app_classroom/views.py
+++ app_classroom/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from datetime import datetime
 from .models import Student
 from django.db.models import Min
+from django.db.models.functions import TruncDate
 
 # data = [
 #     {'id':1,'room':1,'date':datetime(2024,8,1),'time':'120000','image':'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACFSURBVHgBXVBJEsMwCBMe//+xIVxLWUSc9sAwgAQScgEfE4ECMPfKEXJHZniErCw0ANaAJvSwspGwE50N7W0yGyYrwZuNnzN2es5LvsnCHxizbfpLD7ODxqgRynq/NeXgamPlugis9+MumAma83T/1Gs0vEClL4k0dt7Ds/OScnk3cf6JL1E8ilsFjEzgAAAAAElFTkSuQmCC'},
@@ -18,26 +18,29 @@ from django.db.models import Min
 #     print(context)
 #     return render(request,'app_classroom/classrooms.html',context)
 
+
 def classrooms(request):
-    first_images = Student.objects.values('room').annotate(
-        min_date=Min('date')
-    )
-    first_image_records = []
-    for img in first_images:
-        record = Student.objects.get(room=img['room'], date=img['min_date'])
-        first_image_records.append(record)
-    
+    first_images = Student.objects.annotate(
+        truncated_date=TruncDate('date')
+    ).values('room', 'truncated_date').annotate(
+        min_id=Min('id')
+    ).values('min_id')
+    first_image_records = Student.objects.filter(id__in=first_images)
+
     context = {'classrooms': first_image_records}
     return render(request, 'app_classroom/classrooms.html', context)
 
-def classroom(request,classroom_id):
-    print("classroom input id:",classroom_id)
+def classroom(request, classroom_id):
+    print("->classroom input id:", classroom_id)
     selected_image = Student.objects.get(id=classroom_id)
-    # print('-> selected_image',selected_image)
+    
+    truncated_date = selected_image.date.date()
+    
     images = Student.objects.filter(
-                                    room = selected_image.room,
+        room=selected_image.room,
+        date__date=truncated_date
     ).order_by('date')
-    # print('-> images',images)
+    
     context = {'images': images, 'selected_image': selected_image}
     return render(request, 'app_classroom/classroom.html', context)
 
